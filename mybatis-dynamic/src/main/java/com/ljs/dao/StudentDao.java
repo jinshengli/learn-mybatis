f0b8dc5ad9c8782e533b639ee8d469a4d7d7bc83
jinshengli <1515026890@qq.com>
2020-06-11 16:35:05 +0800

    参数传递

--- mybatis-dynamic/src/main/java/com/ljs/dao/StudentDao.java
+++ mybatis-dynamic/src/main/java/com/ljs/dao/StudentDao.java
@@ -2,15 +2,17 @@ package com.ljs.dao;
 
 
 import com.ljs.entity.Student;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * create by ljs on 2020/6/9 8:55
  */
 public interface StudentDao {
 
-    List<Student> findAllStudent();
+    List<Student> findAllStudent(String name);
 
     Student findStudentById(Integer id);
 
@@ -19,5 +21,14 @@ public interface StudentDao {
     int updateStudent(Student student);
 
     int deleteStudent(Integer id);
+
+    // 以下为了测试传参的情况
+
+    // 声明参数类型
+    List<Student> findStudentByIdOrName(@Param("id") Integer id, @Param("name") String name);
+    // 不声明，在映射文件中 需要指定参数顺序。 #{arg0}, #{arg1}
+    List<Student> findStudentByNameOrAge(String name, Integer age);
+    // 传递map, key需要明确。
+    List<Student> findStudentByMap(Map<String, Object> map);
 
 }
